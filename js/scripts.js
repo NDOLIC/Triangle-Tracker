c8438058f1b1b654eb6446b11562d34df98d6145
NDOLIC <tblaguese1@gmail.com>
2018-12-14 13:24:16 +0200

    equilatel

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,9 @@ var side1 = parseInt(document.getElementById("side1").value);
 var side2 = parseInt(document.getElementById("side2").value);
 var side3 = parseInt(document.getElementById("side3").value);
 if((side1+side2)>side3 || (side2 + side3)>side1 ||(side1 + side3)>side2){
-    alert("Triangle");
+    alert("Triangle");{
+        if(side1===side2&&side2===side3&&side3===side1){
+            alert("EQUILATERAL");
+}
 }
 }
