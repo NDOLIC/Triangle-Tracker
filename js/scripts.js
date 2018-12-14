65b0d4f8df1a5efe387d0d652e3df36d26b96a6c
NDOLIC <tblaguese1@gmail.com>
2018-12-14 13:27:20 +0200

    isoscele

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,8 @@ if((side1+side2)>side3 || (side2 + side3)>side1 ||(side1 + side3)>side2){
         if(side1===side2&&side2===side3&&side3===side1){
             alert("EQUILATERAL");
 }
+else if((side1 === side2)||(side2 === side3)||(side3 === side1)){
+    alert("ISOSCELES");
+    }
 }
 }
