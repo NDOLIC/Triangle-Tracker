1756709fc0e0e32fc8cd09c3842d1539425f7d26
NDOLIC <tblaguese1@gmail.com>
2018-12-14 15:07:27 +0200

    linking css

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,30 @@
-function triangle(){
-var side1 = parseInt(document.getElementById("side1").value);
-var side2 = parseInt(document.getElementById("side2").value);
-var side3 = parseInt(document.getElementById("side3").value);
-if((side1+side2)>side3 || (side2 + side3)>side1 ||(side1 + side3)>side2){
-    alert("Triangle");{
-        if(side1===side2&&side2===side3&&side3===side1){
-        alert("EQUILATERAL");
-        }
-else if((side1 === side2)||(side2 === side3)||(side3 === side1)){
-    alert("ISOSCELES");
+function triangle()
+{
+    var side1 = parseInt(document.getElementById("side1").value);
+    var side2 = parseInt(document.getElementById("side2").value);
+    var side3 = parseInt(document.getElementById("side3").value);
+    if((side1+side2)>side3 || (side2 + side3)>side1 ||(side1 + side3)>side2)
+    {
+       alert("Triangle");
+       if((side1===side2)&&(side2===side3)&&(side3===side1))
+       {
+       alert("EQUILATERAL");
+       }
+       else if((side1 === side2)||(side2 === side3)||(side3 === side1))
+       {
+         alert("ISOSCELES");
+       }
+       else if((side1!=side2!=side3) && ((side1+nside2)||(side1+side3)|| (side2+side3))>side3)
+             {
+                alert("SCALENE");
+             }
+       else
+       {
+         alert("unknown type");
+       }
     }
-    else if(side1!==side2 &&side2!== side3&& side3!== side1){
-        alert("SCALENE");
+    else
+    {
+      alert("those sides can't make triangle"); 
     }
-    else{
-        alert("unknown type");
-         }
-     }
-     }
-     else{
-         alert("those sides can't make triangle"); 
-}
-}
+};
