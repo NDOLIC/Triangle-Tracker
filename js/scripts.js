92d73491791d1eac718bcd40719123df8e1a0e88
NDOLIC <tblaguese1@gmail.com>
2018-12-17 08:28:52 +0200

    comments in html

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function triangle()
     var side3 = parseInt(document.getElementById("side3").value);
     if((side1+side2)>side3 && (side2 + side3)>side1 && (side1 + side3)>side2)
     {
-       alert("Triangle");
+       alert("those sides makes a Triangle continue clicking on ok to see which type");
        if(side1===side2 && side2===side3 && side3===side1)
        {
        alert("EQUILATERAL");
